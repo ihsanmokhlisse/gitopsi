f089f76153c4bbf6fad898410992436e6ea77fe5
ihsan mokhlisse <ihsanmokhlisse@gmail.com>
2025-12-17 20:56:04 +0100

    feat(env): Flexible environment and cluster management (#17)
    Implements Issue #17: Flexible environment and cluster management
    
    ## Features
    - Environment topologies: namespace-based, cluster-per-env, multi-cluster
    - CLI commands: env create/list/show/delete, add-cluster/remove-cluster, promote
    - ArgoCD multi-cluster ApplicationSets with cluster generators
    - Comprehensive config file support for all topologies
    
    ## Changes
    - New internal/environment package
    - New CLI commands in internal/cli/env.go
    - Updated ArgoCD generator for multi-cluster support
    - New ArgoCD templates for cluster-based ApplicationSets
    - Unit tests for environment and manager operations
    
    Closes #17

--- internal/generator/argocd.go
+++ internal/generator/argocd.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"fmt"
 
+	"github.com/ihsanmokhlisse/gitopsi/internal/config"
 	"github.com/ihsanmokhlisse/gitopsi/internal/templates"
 )
 
@@ -31,6 +32,18 @@ func (g *Generator) generateArgoCD() error {
 
 	argoCDNamespace := g.getArgoCDNamespace()
 
+	if err := g.generateArgoCDProjects(argoCDNamespace); err != nil {
+		return err
+	}
+
+	if g.Config.IsMultiCluster() {
+		return g.generateMultiClusterArgoCD(argoCDNamespace)
+	}
+
+	return g.generateSingleClusterArgoCD(argoCDNamespace)
+}
+
+func (g *Generator) generateArgoCDProjects(argoCDNamespace string) error {
 	if g.Config.Scope == "infrastructure" || g.Config.Scope == "both" {
 		projectData := map[string]string{
 			"Name":            "infrastructure",
@@ -63,6 +76,10 @@ func (g *Generator) generateArgoCD() error {
 		}
 	}
 
+	return nil
+}
+
+func (g *Generator) generateSingleClusterArgoCD(argoCDNamespace string) error {
 	repoURL := g.Config.Output.URL
 	if repoURL == "" {
 		repoURL = "https://github.com/org/" + g.Config.Project.Name + ".git"
@@ -111,6 +128,168 @@ func (g *Generator) generateArgoCD() error {
 			if err := g.Writer.WriteFile(path, content); err != nil {
 				return err
 			}
+		}
+	}
+
+	return nil
+}
+
+func (g *Generator) generateMultiClusterArgoCD(argoCDNamespace string) error {
+	repoURL := g.Config.Output.URL
+	if repoURL == "" {
+		repoURL = "https://github.com/org/" + g.Config.Project.Name + ".git"
+	}
+
+	branch := g.Config.Output.Branch
+	if branch == "" {
+		branch = "main"
+	}
+
+	if err := g.generateClusterSecrets(argoCDNamespace); err != nil {
+		return err
+	}
+
+	switch g.Config.Topology {
+	case config.TopologyClusterPerEnv:
+		return g.generateClusterPerEnvApplicationSets(argoCDNamespace, repoURL, branch)
+	case config.TopologyMultiCluster:
+		return g.generateMultiClusterApplicationSets(argoCDNamespace, repoURL, branch)
+	}
+
+	return nil
+}
+
+func (g *Generator) generateClusterSecrets(argoCDNamespace string) error {
+	for _, env := range g.Config.Environments {
+		for _, cluster := range env.Clusters {
+			secretData := map[string]any{
+				"Name":            cluster.Name,
+				"URL":             cluster.URL,
+				"Environment":     env.Name,
+				"Region":          cluster.Region,
+				"Primary":         cluster.Primary,
+				"ArgoCDNamespace": argoCDNamespace,
+			}
+			content, err := templates.Render("argocd/cluster-secret.yaml.tmpl", secretData)
+			if err != nil {
+				return err
+			}
+			path := fmt.Sprintf("%s/%s/clusters/%s.yaml",
+				g.Config.Project.Name, g.Config.GitOpsTool, cluster.Name)
+			if err := g.Writer.WriteFile(path, content); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (g *Generator) generateClusterPerEnvApplicationSets(argoCDNamespace, repoURL, branch string) error {
+	for _, env := range g.Config.Environments {
+		namespace := g.Config.GetEnvironmentNamespace(env.Name)
+
+		if g.Config.Scope == "infrastructure" || g.Config.Scope == "both" {
+			appSetData := map[string]any{
+				"Name":            g.Config.Project.Name + "-infra",
+				"Environment":     env.Name,
+				"Project":         "infrastructure",
+				"RepoURL":         repoURL,
+				"Branch":          branch,
+				"Path":            "infrastructure",
+				"Namespace":       namespace,
+				"ArgoCDNamespace": argoCDNamespace,
+			}
+			content, err := templates.Render("argocd/applicationset-cluster.yaml.tmpl", appSetData)
+			if err != nil {
+				return err
+			}
+			path := fmt.Sprintf("%s/%s/applicationsets/infra-%s-cluster.yaml",
+				g.Config.Project.Name, g.Config.GitOpsTool, env.Name)
+			if err := g.Writer.WriteFile(path, content); err != nil {
+				return err
+			}
+		}
+
+		if g.Config.Scope == "application" || g.Config.Scope == "both" {
+			appSetData := map[string]any{
+				"Name":            g.Config.Project.Name + "-apps",
+				"Environment":     env.Name,
+				"Project":         "applications",
+				"RepoURL":         repoURL,
+				"Branch":          branch,
+				"Path":            "applications",
+				"Namespace":       namespace,
+				"ArgoCDNamespace": argoCDNamespace,
+			}
+			content, err := templates.Render("argocd/applicationset-cluster.yaml.tmpl", appSetData)
+			if err != nil {
+				return err
+			}
+			path := fmt.Sprintf("%s/%s/applicationsets/apps-%s-cluster.yaml",
+				g.Config.Project.Name, g.Config.GitOpsTool, env.Name)
+			if err := g.Writer.WriteFile(path, content); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+type envInfo struct {
+	Name      string
+	Namespace string
+}
+
+func (g *Generator) generateMultiClusterApplicationSets(argoCDNamespace, repoURL, branch string) error {
+	envList := make([]envInfo, 0, len(g.Config.Environments))
+	for _, env := range g.Config.Environments {
+		envList = append(envList, envInfo{
+			Name:      env.Name,
+			Namespace: g.Config.GetEnvironmentNamespace(env.Name),
+		})
+	}
+
+	if g.Config.Scope == "infrastructure" || g.Config.Scope == "both" {
+		appSetData := map[string]any{
+			"Name":            g.Config.Project.Name + "-infra",
+			"Environments":    envList,
+			"Project":         "infrastructure",
+			"RepoURL":         repoURL,
+			"Branch":          branch,
+			"Path":            "infrastructure",
+			"ArgoCDNamespace": argoCDNamespace,
+		}
+		content, err := templates.Render("argocd/applicationset-matrix.yaml.tmpl", appSetData)
+		if err != nil {
+			return err
+		}
+		path := fmt.Sprintf("%s/%s/applicationsets/infra-multi-cluster.yaml",
+			g.Config.Project.Name, g.Config.GitOpsTool)
+		if err := g.Writer.WriteFile(path, content); err != nil {
+			return err
+		}
+	}
+
+	if g.Config.Scope == "application" || g.Config.Scope == "both" {
+		appSetData := map[string]any{
+			"Name":            g.Config.Project.Name + "-apps",
+			"Environments":    envList,
+			"Project":         "applications",
+			"RepoURL":         repoURL,
+			"Branch":          branch,
+			"Path":            "applications",
+			"ArgoCDNamespace": argoCDNamespace,
+		}
+		content, err := templates.Render("argocd/applicationset-matrix.yaml.tmpl", appSetData)
+		if err != nil {
+			return err
+		}
+		path := fmt.Sprintf("%s/%s/applicationsets/apps-multi-cluster.yaml",
+			g.Config.Project.Name, g.Config.GitOpsTool)
+		if err := g.Writer.WriteFile(path, content); err != nil {
+			return err
 		}
 	}
 
